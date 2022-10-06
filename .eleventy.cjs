31fad961726071d512b263078e2ae4562075f952
Ching <89380886+Chingsan53@users.noreply.github.com>
2022-10-05 19:50:24 -0700

    Update .eleventy.cjs

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,10 +1,4 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.setUseGitIgnore(false)
-  eleventyConfig.setBrowserSyncConfig({
-    notify: true,
-    open: true
-  })
-}
+
 module.exports = {
     dir: {
         input: "src",
